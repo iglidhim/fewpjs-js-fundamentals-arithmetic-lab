5d5ef7cae4bd716da59332b165a9c22deff63209
Igli Dhima <iglidhim@amazon.com>
2021-04-03 22:51:21 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1= 2;
+const num2= 31;
+const multiply= num1 * num2;
+const random= Math.floor( Math.random() * 1000) + 1 ;
+const num3= 6;
+const num4= 16;
+const mod= num4 % num3 ;
+const max= Math.max(5,10,15,20);
